f422035412b883e271b1bffc1647571ecd9a3887
Irfan Akbar <irfanz47@hotmail.com>
2024-10-24 01:35:40 +0500

    changes in main

--- main.tf
+++ main.tf
@@ -6,3 +6,17 @@ terraform {
     }
   }
 }
+#Configure ProxMox API user/permissions and API url
+provider "proxmox" {
+  pm_api_url           = var.pm_api_url
+  pm_api_token_id      = var.pm_api_token_id
+  pm_api_token_secret  = var.pm_api_token_secret
+  pm_tls_insecure      = var.pm_tls_insecure
+}
+# Data source to fetch information
+data "proxmox_nodes" "all" {}
+
+# Output the list of available Proxmox nodes
+output "proxmox_nodes" {
+  value = data.proxmox_nodes.all.names
+}
