b902eb48d57a701f4e3ce5b488bbabe9913092ba
Irfan Akbar <irfanz47@hotmail.com>
2024-10-24 01:58:39 +0500

    fetching nodes

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ terraform {
   }
 }
 #Configure ProxMox API user/permissions and API url
-provider "proxmox" {
-  pm_api_url           = var.pm_api_url
-  pm_api_token_id      = var.pm_api_token_id
-  pm_api_token_secret  = var.pm_api_token_secret
-  pm_tls_insecure      = true
-}
+#provider "proxmox" {
+  #pm_api_url           = var.pm_api_url
+#  pm_api_token_id      = var.pm_api_token_id
+#  pm_api_token_secret  = var.pm_api_token_secret
+#  pm_tls_insecure      = true
+#}
 # Data source to fetch information
 data "proxmox_nodes" "all" {}
 
