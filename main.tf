66848a93376ea4935ce885c25f424822ec764d10
Irfan Akbar <irfanz47@hotmail.com>
2024-10-24 02:10:08 +0500

    testing vm

--- main.tf
+++ main.tf
@@ -14,9 +14,20 @@ terraform {
 #  pm_tls_insecure      = true
 #}
 # Data source to fetch information
-data "proxmox_nodes" "all" {}
-
-# Output the list of available Proxmox nodes
-output "proxmox_nodes" {
-  value = data.proxmox_nodes.all.names
+resource "proxmox_vm_qemu" "test_vm" {
+  name        = "terraform-test-vm"
+  target_node = "pve" # Replace with your Proxmox node
+  vmid        = 9210
+  clone       = "121"
+ # os_type     = "cloud-init"
+  cores       = 2
+  memory      = 1024
+  disk {
+    size = "10G"
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"            # Use your network bridge name
+    tag    = 150                # VLAN ID 150
+  }
 }
