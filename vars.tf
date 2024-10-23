2f5dd144d7d2f52bd4299fbb08a443c16f76e95b
Irfan Akbar <irfanz47@hotmail.com>
2024-10-24 02:55:03 +0500

    vars

--- vars.tf
+++ vars.tf
@@ -1,1 +1,5 @@
-variable "pm_api_url" {}
+variable "pm_api_url" {
+  description = "Proxmox API URL"
+  type        = string
+  default     = "https://64.255.204.91:8006"  # Default value set here
+}
